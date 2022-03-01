b4ce19c6affb9e1af594be99ea3a1d8f91f320e5
vedant prasad priya karle <rays57529@gmail.com>
2022-03-01 18:11:21 +0530

    added share result functionality

--- App.js
+++ App.js
@@ -1,15 +1,143 @@
+import * as Clipboard from "expo-clipboard";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+	Alert,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 import Keyboard from "./src/components/Keyboard";
-import { colors } from "./src/constants";
+import { CLEAR, colors, colorsToEmoji, ENTER } from "./src/constants";
 
 const TRIES = 6;
+
+const copyArray = arr => {
+	return [...arr.map(rows => [...rows])];
+};
 
 export default function App() {
 	const word = "hello";
 	const letters = word.split("");
 
-	const rows = new Array(TRIES).fill(new Array(letters.length).fill("a"));
+	const [rows, setRows] = useState(
+		new Array(TRIES).fill(new Array(letters.length).fill("")),
+	);
+	const [curRow, setCurRow] = useState(0);
+	const [curCol, setCurCol] = useState(0);
+	const [gameState, setGameState] = useState("playing");
+
+	const onKeyPressed = key => {
+		if (gameState !== "playing") return;
+
+		const updatedRows = copyArray(rows);
+
+		if (key === CLEAR) {
+			const prevCol = curCol - 1;
+			if (prevCol >= 0) {
+				updatedRows[curRow][prevCol] = "";
+				setRows(updatedRows);
+				setCurCol(prevCol);
+			}
+			return;
+		}
+
+		if (key === ENTER) {
+			if (curCol === rows[0].length) {
+				setCurRow(prev => prev + 1);
+				setCurCol(0);
+			}
+			return;
+		}
+
+		if (curCol < rows[0].length) {
+			updatedRows[curRow][curCol] = key;
+			setRows(updatedRows);
+			setCurCol(prev => prev + 1);
+		}
+	};
+
+	const isCellActive = (row, col) => {
+		return row === curRow && col === curCol;
+	};
+
+	const getCellBGColor = (row, col) => {
+		const letter = rows[row][col];
+
+		if (row >= curRow) {
+			return colors.black;
+		}
+
+		if (letter === letters[col]) {
+			return colors.primary;
+		}
+		if (letters.includes(letter)) {
+			return colors.secondary;
+		}
+		return colors.darkgrey;
+	};
+
+	const greenCaps = rows.flatMap((row, i) =>
+		row.filter((cell, j) => getCellBGColor(i, j) === colors.primary),
+	);
+
+	const yellowCaps = rows.flatMap((row, i) =>
+		row.filter((cell, j) => getCellBGColor(i, j) === colors.secondary),
+	);
+
+	const greyCaps = rows.flatMap((row, i) =>
+		row.filter((cell, j) => getCellBGColor(i, j) === colors.darkgrey),
+	);
+
+	const shareResult = () => {
+		const textMap = rows
+			.map((row, i) =>
+				row.map((cell, j) => colorsToEmoji[getCellBGColor(i, j)]).join(""),
+			)
+			.filter(row => row)
+			.join("\n");
+
+		const textToShare = `WORDLE \n${textMap}`;
+
+		Clipboard.setString(textToShare);
+		Alert.alert(
+			"Copied Successfully",
+			"Share your score on your social media!",
+		);
+	};
+
+	const checkHameState = () => {
+		if (checkIfWon()) {
+			Alert.alert("Hurray!", "You Won!", [
+				{
+					text: "Share",
+					onPress: shareResult,
+				},
+			]);
+			setGameState("won");
+		} else if (checkIfLost()) {
+			Alert.alert("Meh!", "Try again tomorrow!");
+			setGameState("lost");
+		}
+	};
+
+	const checkIfWon = () => {
+		const row = rows[curRow - 1];
+
+		return row.every((letter, i) => letter === letters[i]);
+	};
+
+	const checkIfLost = () => {
+		return curRow === rows.length;
+	};
+
+	useEffect(() => {
+		if (curRow > 0) {
+			checkHameState();
+		}
+	}, [curRow]);
 
 	return (
 		<SafeAreaView style={styles.container}>
@@ -18,9 +146,19 @@ export default function App() {
 
 			<ScrollView style={styles.map}>
 				{rows.map((r, index) => (
-					<View style={styles.row}>
+					<View style={styles.row} key={`row-${index}`}>
 						{r.map((l, i) => (
-							<View style={styles.cell} key={i}>
+							<View
+								style={[
+									styles.cell,
+									{
+										borderColor: isCellActive(index, i)
+											? colors.grey
+											: colors.darkgrey,
+										backgroundColor: getCellBGColor(index, i),
+									},
+								]}
+								key={i}>
 								<Text style={styles.cellText}>{l.toUpperCase()}</Text>
 							</View>
 						))}
@@ -28,7 +166,12 @@ export default function App() {
 				))}
 			</ScrollView>
 
-			<Keyboard />
+			<Keyboard
+				onKeyPressed={onKeyPressed}
+				greenCaps={greenCaps}
+				yellowCaps={yellowCaps}
+				greyCaps={greyCaps}
+			/>
 		</SafeAreaView>
 	);
 }
@@ -54,7 +197,6 @@ const styles = StyleSheet.create({
 	},
 	map: {
 		alignSelf: "stretch",
-		height: 100,
 	},
 	row: {
 		alignSelf: "stretch",
